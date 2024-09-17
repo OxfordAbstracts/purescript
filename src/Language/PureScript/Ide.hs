cedeb8a7fb35a3418861f8b07f7686d4914a6ae9
Rory Campbell <roryc89@googlemail.com>
2024-09-17 11:52:21 +0200

    adds HandlerM and IdeM

--- src/Language/PureScript/Ide.hs
+++ src/Language/PureScript/Ide.hs
@@ -16,6 +16,7 @@
 
 module Language.PureScript.Ide
        ( handleCommand
+       , loadModulesAsync
        ) where
 
 import Protolude hiding (moduleName)
--- src/Language/PureScript/LspSimple.hs
+++ src/Language/PureScript/LspSimple.hs
@@ -1,20 +1,34 @@
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 
 module Language.PureScript.LspSimple where
 
+import Control.Monad.IO.Unlift
+import Control.Monad.Reader (mapReaderT)
 import Data.Text qualified as T
 import Language.LSP.Protocol.Message qualified as Message
 import Language.LSP.Protocol.Types qualified as Types
 import Language.LSP.Server qualified as Server
-import Language.PureScript.Ide.Types (IdeEnvironment)
+import Language.PureScript.Ide (loadModulesAsync)
+import Language.PureScript.Ide.Error (IdeError)
+import Language.PureScript.Ide.Types (IdeConfiguration (confLogLevel), IdeEnvironment (ideConfiguration))
+import Language.PureScript.Ide.Util (runLogger)
 import Protolude
+import "monad-logger" Control.Monad.Logger (LoggingT, mapLoggingT)
 
-type HandlerM = Server.LspT () (ReaderT IdeEnvironment IO)
+type HandlerM config = Server.LspT config (ReaderT IdeEnvironment (LoggingT IO))
 
-handlers :: Server.Handlers HandlerM
+type IdeM = ReaderT IdeEnvironment (LoggingT (ExceptT IdeError IO))
+
+runIde :: IdeM a -> HandlerM config (Either IdeError a)
+runIde = lift . mapReaderT (mapLoggingT runExceptT)
+
+handlers :: Server.Handlers (HandlerM ())
 handlers =
   mconcat
     [ Server.notificationHandler Message.SMethod_Initialized $ \_not -> do
@@ -45,19 +59,27 @@ handlers =
             range = Types.Range pos pos
         responder (Right $ Types.InL rsp),
       Server.notificationHandler Message.SMethod_TextDocumentDidOpen $ \msg -> do
+        res <- runIde $ loadModulesAsync []
+        -- publishD
         pure ()
     ]
 
--- main :: IO Int
--- main =
---   Server.runServer $
---     Server.ServerDefinition
---       { parseConfig = const $ const $ Right (),
---         onConfigChange = const $ pure (),
---         defaultConfig = (),
---         configSection = "oa-purescript",
---         doInitialize = \env _req -> pure $ Right env,
---         staticHandlers = \_caps -> handlers,
---         interpretHandler = \env -> Server.Iso (Server.runLspT env) liftIO,
---         options = Server.defaultOptions
---       }
+main :: IdeEnvironment -> IO Int
+main ideEnv =
+  Server.runServer $
+    Server.ServerDefinition
+      { parseConfig = const $ const $ Right (),
+        onConfigChange = const $ pure (),
+        defaultConfig = (),
+        configSection = "oa-purescript",
+        doInitialize = \env _req -> pure $ Right env,
+        staticHandlers = \_caps -> handlers,
+        interpretHandler = \env ->
+          Server.Iso
+            ( runLogger (confLogLevel (ideConfiguration ideEnv))
+                . flip runReaderT ideEnv
+                . Server.runLspT env
+            )
+            liftIO,
+        options = Server.defaultOptions
+      }
