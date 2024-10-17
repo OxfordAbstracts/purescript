adabb521dae9e1635f0d90d31988f42003bb2406
Rory Campbell <roryc89@googlemail.com>
2024-10-17 12:33:02 +0200

    use record syntax for unHandlerM

--- src/Language/PureScript/Lsp/Monad.hs
+++ src/Language/PureScript/Lsp/Monad.hs
@@ -3,15 +3,17 @@
 module Language.PureScript.Lsp.Monad where
 
 import Control.Monad.Base (MonadBase (liftBase))
+import Control.Monad.Catch (MonadThrow)
 import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Control.Monad.Trans.Control (MonadBaseControl (StM, liftBaseWith, restoreM), RunInBase)
 import Language.LSP.Server (LanguageContextEnv, LspT (LspT), MonadLsp, runLspT)
 import Language.PureScript.Lsp.ServerConfig (ServerConfig)
 import Language.PureScript.Lsp.Types
 import Protolude
-import Control.Monad.Catch (MonadThrow)
 
-newtype HandlerM a = HandlerM (ReaderT LspEnvironment (LspT ServerConfig IO) a)
+newtype HandlerM a = HandlerM
+  { unHandlerM :: ReaderT LspEnvironment (LspT ServerConfig IO) a
+  }
   deriving (Functor, Applicative, Monad, MonadIO, MonadUnliftIO, MonadThrow, MonadReader LspEnvironment, MonadLsp ServerConfig)
 
 instance MonadBase IO HandlerM where
@@ -30,9 +32,6 @@ instance MonadBaseControl IO HandlerM where
 
   restoreM :: StM HandlerM a -> HandlerM a
   restoreM = pure
-
-unHandlerM :: HandlerM a -> ReaderT LspEnvironment (LspT ServerConfig IO) a
-unHandlerM (HandlerM a) = a
 
 runHandlerM :: LanguageContextEnv ServerConfig -> LspEnvironment -> HandlerM a -> IO a
 runHandlerM env lspEnv (HandlerM a) = runLspT env $ runReaderT a lspEnv
