a2fc356d86763bd9d808a5f24b5bdadd9b1b53c5
Brad Reed <brad@bradreed.co.uk>
2026-03-18 15:05:28 +0800

    failing test

--- tests/TestUtils.hs
+++ tests/TestUtils.hs
@@ -21,6 +21,7 @@ import Data.Char (isSpace)
 import Data.Function (on)
 import Data.List (sort, sortBy, stripPrefix, groupBy, find)
 import Data.Map qualified as M
+import Data.Set (Set)
 import Data.Maybe (isJust)
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as T
@@ -146,7 +147,7 @@ setupSupportModules = do
   ms <- getSupportModuleTuples
   let modules = map snd ms
   supportExterns <- runExceptT $ do
-    foreigns <- inferForeignModules ms
+    foreigns <- inferForeignModules (P.optionsFFIExts P.defaultOptions) ms
     externs <- ExceptT . fmap fst . runTest $ P.make (makeActions modules foreigns) (CST.pureResult <$> modules)
     return (externs, foreigns)
   case supportExterns of
@@ -204,7 +205,7 @@ compile' options expectedModule SupportModules{..} inputFiles = do
     msWithWarnings <- CST.parseFromFiles id fs
     tell $ foldMap (\(fp, (ws, _)) -> CST.toMultipleWarnings fp ws) msWithWarnings
     let ms = fmap snd <$> msWithWarnings
-    foreigns <- inferForeignModules ms
+    foreigns <- inferForeignModules (P.optionsFFIExts options) ms
     let
       actions = makeActions supportModules (foreigns `M.union` supportForeigns)
       (hasExpectedModuleName, expectedModuleName, compiledModulePath) = case expectedModule of
@@ -263,9 +264,10 @@ runTest = P.runMake P.defaultOptions
 
 inferForeignModules
   :: MonadIO m
-  => [(FilePath, P.Module)]
+  => Set String
+  -> [(FilePath, P.Module)]
   -> m (M.Map P.ModuleName FilePath)
-inferForeignModules = P.inferForeignModules (P.optionsFFIExts P.defaultOptions) . fromList
+inferForeignModules exts = P.inferForeignModules exts . fromList
   where
     fromList :: [(FilePath, P.Module)] -> M.Map P.ModuleName (Either P.RebuildPolicy FilePath)
     fromList = M.fromList . map ((P.getModuleName *** Right) . swap)
